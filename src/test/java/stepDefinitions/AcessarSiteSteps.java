4e63e341041713f154554aa600124ba6b765dd75
Lebarc <le27barcellos@gmail.com>
2022-11-19 16:08:33 -0300

    Ajuste na class cucumberHook - assert refatorado

--- src/test/java/stepDefinitions/AcessarSiteSteps.java
+++ src/test/java/stepDefinitions/AcessarSiteSteps.java
@@ -5,8 +5,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import testActions.NavegarActions;
 
-import static org.junit.Assert.assertTrue;
-
 public class AcessarSiteSteps {
 
     NavegarActions Navegar = new NavegarActions();
@@ -14,30 +12,25 @@ public class AcessarSiteSteps {
     @Given("Acesso o portal {string}")
     public void acesso_o_portal(String url) {
         utils.ClassInfo.GetInstance().setResultScenario(Navegar.acessarSite(url));
-        assertTrue(utils.ClassInfo.GetInstance().getLogMessage(), utils.ClassInfo.GetInstance().getResultScenario());
     }
 
     @When("Clico em elements")
     public void clicoEmElements() {
         utils.ClassInfo.GetInstance().setResultScenario(Navegar.ClicarElements());
-        assertTrue(utils.ClassInfo.GetInstance().getLogMessage(), utils.ClassInfo.GetInstance().getResultScenario());
     }
 
     @Then("Visualizo a página Elements")
     public void visualizoAPáginaElements() {
         utils.ClassInfo.GetInstance().setResultScenario(Navegar.ValidarDirecionamentoParaElementsPage());
-        assertTrue(utils.ClassInfo.GetInstance().getLogMessage(), utils.ClassInfo.GetInstance().getResultScenario());
     }
 
     @When("Clico em Forms")
     public void clicoEmForms() {
         utils.ClassInfo.GetInstance().setResultScenario(Navegar.ClicarForms());
-        assertTrue(utils.ClassInfo.GetInstance().getLogMessage(), utils.ClassInfo.GetInstance().getResultScenario());
     }
 
     @Then("Visualizo a página Forms")
     public void visualizoAPáginaForms() {
         utils.ClassInfo.GetInstance().setResultScenario(Navegar.ValidarDirecionamentoParaFormsPage());
-        assertTrue(utils.ClassInfo.GetInstance().getLogMessage(), utils.ClassInfo.GetInstance().getResultScenario());
     }
 }
--- src/test/java/stepDefinitions/CucumberHook.java
+++ src/test/java/stepDefinitions/CucumberHook.java
@@ -1,15 +1,26 @@
 package stepDefinitions;
 
+import io.cucumber.core.gherkin.Feature;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.BeforeTest;
 import testRunner.TestRun;
+
+import static org.junit.Assert.assertTrue;
 
 public class CucumberHook extends TestRun {
 
+    @BeforeTest
+    public void BeforeFeature(Feature feature) {
+
+    }
+
     @After
     public void AfterMethod(Scenario scenario) {
+        assertTrue(utils.ClassInfo.GetInstance().getLogMessage(), utils.ClassInfo.GetInstance().getResultScenario());
+
         if (scenario.isFailed()) {
             byte[] data = ((TakesScreenshot) utils.ClassDriver.GetInstance().Driver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(data, "image/png", scenario.getName());
